39ac58ce2980282c533157fe2243b6fe55e4e119
Robert Tublen <robert.tublen@junction7.se>
2018-09-25 09:44:33 +0200

    test2

--- T800/Program.cs
+++ T800/Program.cs
@@ -6,7 +6,7 @@ namespace T800
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Bla bla");
+            Console.WriteLine("Hello World! Bla ");
         }
     }
 }
